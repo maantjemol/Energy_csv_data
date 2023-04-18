a742ace5580baf50d49a2eeb85403bbdaf28a016
Maantje Mol <35200312+maantjemol@users.noreply.github.com>
2023-04-18 12:07:52 +0200

    first commit

---
+++ fundaData.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def fetchFundaData (postcode: str):
+  postcode = postcode.lower()
+  postcode = postcode.replace(" ", "")
+  burp0_url = getUrl(postcode)
+  burp0_headers = {"Sec-Ch-Ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"104\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"macOS\"", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate", "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"}
+  fundata =requests.get(burp0_url, headers=burp0_headers)
+  
+  soup = BeautifulSoup(fundata.text, "html.parser")
+  span = soup.select('span[data-instant-search-output="searchButtonTotal"]')
+  if span:
+    print(int(span[0].text.replace(" bouwgrond", "")), postcode)
+    return int(span[0].text.replace(" bouwgrond", ""))
+  else:
+    print("no data")
+    return 0
+
+df = pd.read_csv("test.csv")
+
+
+def getUrl(zip):
+  zip = zip.lower()
+  zip = zip.replace(" ", "")
+  return f"https://www.fundainbusiness.nl/bouwgrond/{zip}/+2km/"
+
+df["funda_url"] = df.apply(lambda x: getUrl(x["zip_code"]), axis=1)
+df["available"] = df.apply(lambda x: fetchFundaData(x["zip_code"]), axis=1)
+csv_data = df.to_csv()
+
+with open("fundata.csv", "w") as f:
+  f.write(csv_data)
---
+++ parseDataFile.py
@@ -0,0 +1,43 @@
+import geopandas as gpd
+import geopy
+import pandas as pd
+
+def get_zip_code(point, geolocator):
+    location = geolocator.reverse(f"{point.y}, {point.x}")
+    print(location.raw['address']['postcode'])
+    return location.raw['address']['postcode']
+
+def parseDataFile(filename: str):
+  data = gpd.read_file(filename)
+
+  data["beschikbare_capaciteit_invoeding_huidig_mva"] = pd.to_numeric(data['beschikbare_capaciteit_invoeding_huidig_mva'],errors='coerce')
+  data["beschikbare_capaciteit_afname_huidig_mva"] = pd.to_numeric(data['beschikbare_capaciteit_afname_huidig_mva'],errors='coerce')
+
+
+  isExist = data["status"] != "Gepland, locatie onbekend"
+  isInvoeding = data["beschikbare_capaciteit_invoeding_huidig_mva"].notnull() 
+  isAfname = data["beschikbare_capaciteit_afname_huidig_mva"].notnull() 
+  isInvoedingGd0 = data["beschikbare_capaciteit_invoeding_huidig_mva"] > 0
+  isAfnameGd0 = data["beschikbare_capaciteit_afname_huidig_mva"] > 0
+
+  data = data[isExist]
+  data = data[isInvoeding]
+  data = data[isAfname]
+  data = data[isInvoedingGd0]
+  data = data[isAfnameGd0]
+
+  data = data.to_crs(epsg=4326)
+
+  geolocator = geopy.Nominatim(user_agent="check_1")
+  data["zip_code"] = data.apply(lambda x: get_zip_code(x.geometry.centroid, geolocator), axis=1)
+
+  csv_data = data.to_csv().replace("POINT ", "")
+  #write data to file:
+  with open("test.csv", "w") as f:
+    f.write(csv_data)
+
+
+
+
+
+parseDataFile("beschikbare_capaciteit_elektriciteitsnet.gpkg")
